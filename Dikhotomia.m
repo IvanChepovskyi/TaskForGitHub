ec80f03c2d95317b91fb6501077ab28c07b488a2
agentvanya <agentvanya@gmail.com>
2016-07-03 17:17:04 +0300

    create method of Dikhotomia

--- Dikhotomia.m
+++ Dikhotomia.m
@@ -2,4 +2,23 @@ function Dikhotomia
 function y=f(x)
 y=(x+1)^2;
 end
+function y=D(a,b,eps)
+if (round(abs(b-a)*10000)/10000>eps)
+    while(round(abs(b-a)*10000)/10000>eps)
+        t=0.5*(a+b-eps);
+        z=0.5*(a+b+eps);
+        ft=f(t);
+        fz=f(z);
+        if(ft<=fz)
+            b=z;
+        else
+            a=t;
+        x=0.5*(a+b);
+        end
+    end
+else
+    x=0.5*(a+b);
+end
+y=x;
+end
 end
